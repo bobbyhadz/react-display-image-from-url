a3e027ae3b14b03ef2459c01f302a03541363f18
bobbyhadz <opmftw@gmail.com>
2024-04-06 16:19:53 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <img
+        src="https://bobbyhadz.com/images/blog/react-prevent-multiple-button-clicks/thumbnail.webp"
+        alt="car"
+      />
+    </div>
+  );
+}
